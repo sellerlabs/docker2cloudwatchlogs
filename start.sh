9e3a3ea0f0cb26fe03275adf2c75586b7339dc76
Brian Whigham <brian@roundsphere.com>
2016-02-25 20:24:15 +0000

    add AWS_REGION environment variable

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-test -n "$AWS_LOGS_CONF_URL" &&  curl $AWS_LOGS_CONF_URL > var/awslogs/etc/awslogs.conf
+test -n "$AWS_LOGS_CONF_URL" &&  curl $AWS_LOGS_CONF_URL > /var/awslogs/etc/awslogs.conf
+test -n "$AWS_REGION" && sed -i "s/us-east-1/$AWS_REGION/" /var/awslogs/etc/aws.conf
 sed -i "s/LOG_STREAM_NAME_PREFIX/$LOG_STREAM_NAME_PREFIX/g" /var/awslogs/etc/awslogs.conf
 sed -i "s/LOG_GROUP_NAME/$LOG_GROUP_NAME/g" /var/awslogs/etc/awslogs.conf
 test -s "$AWS_REGION" && sed -i "s/us-east-1/$AWS_REGION/g" /var/awslogs/etc/aws.conf
